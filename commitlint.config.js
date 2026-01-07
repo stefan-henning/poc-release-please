4c1357bb33d801f2d9c8a7abc41abff50e0d5eb9
Stefan Henning <stefan.henning@fusiongbs.com>
2026-01-07 13:55:54 +0100

    chore: relax casing in commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'release',
+        'feat',
+        'fix',
+        'perf',
+        'deps',
+        'revert',
+        'doc',
+        'style',
+        'chore',
+        'refactor',
+        'test',
+        'build',
+        'ci',
+      ],
+    ],
+    'subject-case': [0],
+  },
 };
