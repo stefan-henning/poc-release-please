9fadec0b6e7a95b0551320fe248faeab7d73a9e0
Stefan Henning <stefan.henning@fusiongbs.com>
2026-01-09 11:43:12 +0100

    chore: reset readme and main.py

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 def main():
     print("Hello from poc-release-please!")
-    # a new feature
-    # refactor code
-    # new package, breaking
-    # perf
-    # adding a feature
 
 if __name__ == "__main__":
     main()
